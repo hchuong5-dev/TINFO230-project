0fe5b6ace1fce9f02aa4d78884cbea7e86bd1b78
hchuong5-dev <hchuong5@uw.edu>
2026-03-03 20:20:05 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function testFunction() {
-    alert("JavaScript is connected.");
-}
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 50) {
+        header.style.background = "rgba(0,0,0,0.8)";
+    } else {
+        header.style.background = "transparent";
+    }
+});
